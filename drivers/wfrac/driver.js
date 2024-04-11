141118f0a904936eb3904f2229ebac343c153cab
Edwin van de Pol <developer@edwinvandepol.nl>
2024-04-11 21:16:05 +0200

    Updated driver

--- drivers/wfrac/driver.js
+++ drivers/wfrac/driver.js
@@ -3,21 +3,6 @@
 const Driver = require('../../lib/Driver');
 
 class WFRACDriver extends Driver {
-
-  /*
-  | Device events
-  */
-
-  // Driver initialized
-  async onInit() {
-    this.log('Initialized');
-  }
-
-  // Driver destroyed
-  async onUninit() {
-    this.log('Destroyed');
-  }
-
 }
 
 module.exports = WFRACDriver;
