db7afeda06a220ff46d0a77e295b55fd4b53b60e
Edwin van de Pol <developer@edwinvandepol.nl>
2024-04-12 15:06:10 +0200

    Updated Homey compatibility

--- app.js
+++ app.js
@@ -2,4 +2,12 @@
 
 const App = require('./lib/App');
 
+// if (process.env.DEBUG === '1') {
+//   try {
+//     require('inspector').waitForDebugger();
+//   } catch (error) {
+//     require('inspector').open(9229, '0.0.0.0', false);
+//   }
+// }
+
 module.exports = App;
